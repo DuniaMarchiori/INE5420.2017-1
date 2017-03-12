f3807080fb99706f36d9a5346c44ccd715de54d2
vsSchweitzer <schweitzer.vinicius@gmail.com>
2017-03-12 10:22:22 -0300

    Update Trabalho1.cpp

--- Trabalho1/Trabalho1.cpp
+++ Trabalho1/Trabalho1.cpp
@@ -1,11 +1,21 @@
+#include <gtk/gtk.h>
+#include <iostream>
+#include <vector>
+
 #include "Ponto.hpp"
 #include "Reta.hpp"
 #include "Poligono.hpp"
 
 #include "Coordenada.hpp"
 
+GtkWidget *window_Main;
 
-//extern "C" G_MODULE_EXPORT void btn_ok_clicked_cb(){ }
+GtkWidget *elmnt_List, *elmnt_Btn_Novo, *elmnt_Btn_Del;
+
+GtkWidget *pos_Txt_Fator, *pos_Btn_Cima, *pos_Btn_Baixo, *pos_Btn_Esq, *pos_Btn_Dir;
+
+GtkWidget *viewport_DrawingArea;
+GtkWidget *window_NovoElemento;
 
 // Main de testes básicos
 int main(int argc, char *argv[]){
@@ -28,5 +38,20 @@ int main(int argc, char *argv[]){
 	Poligono* pol = new Poligono();
 	pol->adicionarCoordenada(c);
 
+	GtkBuilder  *gtkBuilder;
+	gtk_init(&argc, &argv);
+
+	gtkBuilder = gtk_builder_new();
+	gtk_builder_add_from_file(gtkBuilder, "janela.glade", NULL);
+
+	window_Main = GTK_WIDGET(gtk_builder_get_object(GTK_BUILDER(gtkBuilder), "Window_Main"));
+	window_NovoElemento = GTK_WIDGET(gtk_builder_get_object(GTK_BUILDER(gtkBuilder), "Window_NovoElmnt"));
+	viewport_DrawingArea = GTK_WIDGET(gtk_builder_get_object(GTK_BUILDER(gtkBuilder), "Viewport_DrawingArea"));
+	gtk_builder_connect_signals(gtkBuilder, NULL);
+	gtk_widget_show(window_Main);
+	gtk_main ();
+	
 	return 0;
 }
+
+#include "callbacks.hpp"
