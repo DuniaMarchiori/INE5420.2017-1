e4a33a562ce6eaf15c2d36af3d03763cc386c899
vsSchweitzer <schweitzer.vinicius@gmail.com>
2017-03-13 20:24:11 -0300

    Consertando detalhes

--- Trabalho1/Trabalho1.cpp
+++ Trabalho1/Trabalho1.cpp
@@ -103,6 +103,7 @@ int main(int argc, char *argv[]){
 	g_signal_connect (viewport_DrawingArea,"configure-event", G_CALLBACK (viewport_DrawingArea_configure_event_cb), NULL);
 	*/
 	
+	
 	addToListBox(elmnt_List, "Reta_1");
 	
 	gtk_builder_connect_signals(gtkBuilder, NULL);
