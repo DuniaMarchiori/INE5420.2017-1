236f3c7dba7331eb9f034c24b8dcd1427bb3b9de
vsSchweitzer <schweitzer.vinicius@gmail.com>
2017-03-15 22:46:38 -0300

    Adicionado comentários das classes Console e DisplayFile

--- Trabalho1/Console.hpp
+++ Trabalho1/Console.hpp
@@ -4,16 +4,25 @@
 class Console: public ElementoGrafico {
 	
 private:
-	GtkTextView* caixaTexto;
-	GtkTextBuffer *buffer;
+	GtkTextView* caixaTexto; /*!< Referência para a caixa de texto da interface de usuário*/
+	GtkTextBuffer *buffer; /*!< Guarda o texto para ser mostrada na caixa de texto*/
 
 public:
 
+	//! Construtor
+	/*
+		/param _caixaTexto Um ponteiro para a caixa de texto onde este console se mostrará.
+	*/
 	Console(GtkTextView* _caixaTexto) {
 		caixaTexto = _caixaTexto;
 		buffer = gtk_text_view_get_buffer (caixaTexto);
 	}
 	
+	//! Método que adiciona novas mensagens.
+    /*!
+        Adiciona uma nova linha ao console com uma mensagem nova.
+        /param text O texto que sera adicionado ao console.
+    */
 	void inserirTexto(const gchar *texto) {
 		// -1 indica que é para adicionar o texto todo
 		GtkTextIter* iter = new GtkTextIter();
