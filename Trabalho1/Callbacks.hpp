7ead719b6ea94b5cdd044f7c58260b67fd27e836
vsSchweitzer <schweitzer.vinicius@gmail.com>
2017-03-22 00:47:21 -0300

    Metodos do callback migrados para o controller

--- Trabalho1/Callbacks.hpp
+++ Trabalho1/Callbacks.hpp
@@ -3,7 +3,7 @@
 	Finalza o programa quando a janela principal é fechada.
 */
 extern "C" G_MODULE_EXPORT void Window_Main_destroy_cb(){
-	controller->getView()->fecharPrograma();
+	controller->janelaPrincipalDestroy();
 }
 
 //! Método que é chamado ao selecionar um elemtno da list box.
@@ -11,7 +11,7 @@ extern "C" G_MODULE_EXPORT void Window_Main_destroy_cb(){
 	Ativa o botão de deletar elemento.
 */
 extern "C" G_MODULE_EXPORT void Elmnt_List_row_selected_cb (GtkListBox *box, GtkListBoxRow *row, gpointer user_data) {
-	controller->getView()->setElmnt_Btn_DelSensitive(TRUE);
+	controller->selecionaElementoListBox();
 }
 
 //! Método que é chamado ao pressionar o botão de Novo Elemento na janela principal.
@@ -19,26 +19,15 @@ extern "C" G_MODULE_EXPORT void Elmnt_List_row_selected_cb (GtkListBox *box, Gtk
 	Mostra a janela de criação de elemento.
 */
 extern "C" G_MODULE_EXPORT void Elmnt_Btn_Novo_clicked_cb(){
-	controller->getView()->Elmnt_Btn_Novo_Clicado();
+	controller->botaoNovoElemento();
 }
 
 //! Método que é chamado ao pressionar o botão de Deletar um elemento.
 /*!
 	Manda deletar o elemento da display file e executa um update na tela.
 */
-extern "C" G_MODULE_EXPORT void Elmnt_Btn_Del_clicked_cb(){
-	try {
-		int index = controller->getView()->deletarElemento();
-		controller->getModel()->deletarElemento(index);
-		controller->atualizaDesenho();
-		controller->getView()->inserirTextoConsole("Elemento excluído.");
-		controller->getView()->setElmnt_Btn_DelSensitive(FALSE);	
-	} catch (int erro) {
-		if (erro == 1) {
-			controller->getView()->inserirTextoConsole("É preciso selecionar um elemento para ser deletado.");
-		}
-	}
-	
+extern "C" G_MODULE_EXPORT void Elmnt_Btn_Del_clicked_cb(){	
+	controller->botaoDeletarElemento();
 }
 
 //! Método que é chamado ao pressionar o botão "Cima".
@@ -46,14 +35,7 @@ extern "C" G_MODULE_EXPORT void Elmnt_Btn_Del_clicked_cb(){
 	Move a window para cima baseado no fator.
 */
 extern "C" G_MODULE_EXPORT void Pos_Btn_Cima_clicked_cb(){
-	try {
-		double fator = controller->getView()->getFatorMovimento();
-		controller->getModel()->moverWindow(0,fator);
-		controller->atualizaDesenho();
-		controller->getView()->inserirTextoConsole("Movimentação para cima.");
-	} catch (...){
-		return;
-	}
+	controller->botaoMovimentoCima();
 }
 
 
@@ -62,14 +44,7 @@ extern "C" G_MODULE_EXPORT void Pos_Btn_Cima_clicked_cb(){
 	Move a window para esquerda baseado no fator.
 */
 extern "C" G_MODULE_EXPORT void Pos_Btn_Esq_clicked_cb(){
-	try {
-		double fator = controller->getView()->getFatorMovimento();
-		controller->getModel()->moverWindow(-fator,0);
-		controller->atualizaDesenho();
-		controller->getView()->inserirTextoConsole("Movimentação para a esquerda.");
-	} catch (...){
-		return;
-	}
+	controller->botaoMovimentoEsquerda();
 }
 
 
@@ -78,14 +53,7 @@ extern "C" G_MODULE_EXPORT void Pos_Btn_Esq_clicked_cb(){
 	Move a window para direita baseado no fator.
 */
 extern "C" G_MODULE_EXPORT void Pos_Btn_Dir_clicked_cb(){
-	try {
-		double fator = controller->getView()->getFatorMovimento();
-		controller->getModel()->moverWindow(fator,0);
-		controller->atualizaDesenho();
-		controller->getView()->inserirTextoConsole("Movimentação para a direita.");
-	} catch (...){
-		return;
-	}
+	controller->botaoMovimentoDireita();
 }
 
 
@@ -94,14 +62,7 @@ extern "C" G_MODULE_EXPORT void Pos_Btn_Dir_clicked_cb(){
 	Move a window para baixo baseado no fator.
 */
 extern "C" G_MODULE_EXPORT void Pos_Btn_Baixo_clicked_cb(){
-	try {
-		double fator = controller->getView()->getFatorMovimento();
-		controller->getModel()->moverWindow(0,-fator);
-		controller->atualizaDesenho();
-		controller->getView()->inserirTextoConsole("Movimentação para baixo.");
-	} catch (...){
-		return;
-	}
+	controller->botaoMovimentoBaixo();
 }
 
 
@@ -110,14 +71,7 @@ extern "C" G_MODULE_EXPORT void Pos_Btn_Baixo_clicked_cb(){
 	Aumenta o tamanho da window baseado no fator.
 */
 extern "C" G_MODULE_EXPORT void Zoom_Btn_Menos_clicked_cb(){
-	try {
-		double fator = controller->getView()->getFatorZoom();
-		controller->getModel()->zoom(-fator);
-		controller->atualizaDesenho();
-		controller->getView()->inserirTextoConsole("Zoom para fora.");
-	} catch (...){
-		return;
-	}
+	controller->botaoZoomMenos();
 }
 
 //! Método que é chamado ao pressionar o botão "+" do zoom.
@@ -125,14 +79,7 @@ extern "C" G_MODULE_EXPORT void Zoom_Btn_Menos_clicked_cb(){
 	Diminui o tamanho da window baseado no fator.
 */
 extern "C" G_MODULE_EXPORT void Zoom_Btn_Mais_clicked_cb(){
-	try {
-		double fator = controller->getView()->getFatorZoom();
-		controller->getModel()->zoom(fator);
-		controller->atualizaDesenho();
-		controller->getView()->inserirTextoConsole("Zoom para dentro.");
-	} catch (...){
-		return;
-	}
+	controller->botaoZoomMais();
 }
 
 //! Método que é chamado quando o signal "configure event" é ativado.
@@ -140,8 +87,7 @@ extern "C" G_MODULE_EXPORT void Zoom_Btn_Mais_clicked_cb(){
 	Cria uma nova surface a atualiza o desenho.
 */
 extern "C" G_MODULE_EXPORT gboolean Viewport_DrawingArea_configure_event_cb (GtkWidget *widget, GdkEventConfigure *event, gpointer data){
-	controller->getView()->nova_surface(widget);
-	controller->atualizaDesenho();
+	controller->drawingAreaConfigure(widget);
 	return TRUE;
 }
 
@@ -150,7 +96,7 @@ extern "C" G_MODULE_EXPORT gboolean Viewport_DrawingArea_configure_event_cb (Gtk
 	Chama o metodo modifica_surface
 */
 extern "C" G_MODULE_EXPORT gboolean Viewport_DrawingArea_draw_cb (GtkWidget *widget, cairo_t *cr,  gpointer   data){
-	controller->getView()->modifica_surface(cr);
+	controller->drawingAreaDraw(cr);
 	return FALSE;
 }
 
@@ -164,134 +110,7 @@ extern "C" G_MODULE_EXPORT gboolean Viewport_DrawingArea_draw_cb (GtkWidget *wid
 	Verifica qual tipo de objeto esta sendo criado, e manda cria-los na display file, exibindo mensagens dependendo do resultado da operação.
 */
 extern "C" G_MODULE_EXPORT void NovoElmnt_Adicionar_clicked_cb() {
-	
-	string nome = controller->getView()->getNomeElemento();
-	int tipo = controller->getView()->getTipoNovoElemento();
-	switch (tipo) {
-		case 0: { // A page 0 corresponde à aba de Ponto
-			string coordX = controller->getView()->getCoordXNovoPonto();
-			string coordY = controller->getView()->getCoordYNovoPonto();
-			try {
-				controller->getModel()->inserirNovoPonto(nome, coordX, coordY);
-				controller->getView()->limparTextoNovoPonto();
-				controller->getView()->adicionaElementoListbox(nome);
-				controller->getView()->inserirTextoConsole("Novo ponto adicionado.");
-			} catch (int erro) {
-				if (erro == -1) {
-					controller->getView()->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
-				} else if (erro == -2) {
-					controller->getView()->inserirTextoConsole("ERRO: coordenadas devem ser valores numéricos.");
-				} else if (erro == -3) {
-					controller->getView()->inserirTextoConsole("ERRO: não é possível inserir um ponto sem coordenadas.");
-				}
-			}
-			
-			break;
-		} case 1: { // A page 1 corresponde à aba de Reta
-			string coordIniX = controller->getView()->getCoordIniXNovaReta();
-			string coordIniY = controller->getView()->getCoordIniYNovaReta();
-			string coordFinX = controller->getView()->getCoordFinXNovaReta();
-			string coordFinY = controller->getView()->getCoordFinYNovaReta();
-		
-			try {
-				controller->getModel()->inserirNovaReta(nome, coordIniX, coordIniY, coordFinX, coordFinY);
-				controller->getView()->limparTextoNovaReta();
-				controller->getView()->adicionaElementoListbox(nome);
-				controller->getView()->inserirTextoConsole("Nova reta adicionada.");
-			} catch (int erro) {
-				if (erro == -1) {
-					controller->getView()->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
-				} else if (erro == -2) {
-					controller->getView()->inserirTextoConsole("ERRO: coordenadas devem ser valores numéricos.");
-				} else if (erro == -3) {
-					controller->getView()->inserirTextoConsole("ERRO: não é possível inserir reta sem dois pares de coordenadas.");
-				}
-			}
-			
-			break;
-		} case 2: { // A page 2 corresponde à aba de Polígono
-			ListaEnc<Coordenada*>* lista = controller->getView()->getListaCoordsPoligono();
-			
-			try {
-				controller->getModel()->inserirNovoPoligono(nome, lista);
-				controller->getView()->resetarListaCoordenadasPoligono();
-				controller->getView()->limparTextoNovoPoligono();
-				controller->getView()->adicionaElementoListbox(nome);
-				controller->getView()->inserirTextoConsole("Novo poligono adicionado.");
-			} catch (int erro) {
-				if (erro == -1) {
-					controller->getView()->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
-				} else if (erro == -3) {
-					controller->getView()->inserirTextoConsole("ERRO: não é possível inserir polígono sem coordenadas.");
-				}
-			}
-			
-			break;
-		}
-	}
-	
-	controller->atualizaDesenho();
-	controller->getView()->setPoligono_Btn_DelSensitive(FALSE);
-	controller->getView()->focusNome();
-	
-	/*
-	string nome = gtk_entry_get_text(textoNomeElemento);
-	switch (gtk_notebook_get_current_page(notebook)) {
-		case 0: {
-			// A page 0 corresponde à aba de Ponto
-			string coordX = gtk_entry_get_text(textoPontoX);
-			string coordY = gtk_entry_get_text(textoPontoY);
-			int resultado = displayFile->inserirNovoPonto(nome, coordX, coordY);
-			if(resultado == 1) {
-				limparTextoNovoPonto();
-				controller->getView()->inserirTextoConsole("Novo ponto adicionado.");
-			} else if(resultado == -1) {
-				controller->getView()->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
-			} else if(resultado == -2) {
-				controller->getView()->inserirTextoConsole("ERRO: coordenadas devem ser valores numéricos.");
-			} else if(resultado == -3) {
-				controller->getView()->inserirTextoConsole("ERRO: não é possível inserir ponto sem valor X ou Y.");
-			}
-			break;
-			}
-		case 1: {
-			// A page 1 corresponde à aba de Reta
-			string coordIniX = gtk_entry_get_text(textoRetaInicialX);
-			string coordIniY = gtk_entry_get_text(textoRetaInicialY);
-			string coordFinX = gtk_entry_get_text(textoRetaFinalX);
-			string coordFinY = gtk_entry_get_text(textoRetaFinalY);
-			int resultado = displayFile->inserirNovaReta(nome, coordIniX, coordIniY, coordFinX, coordFinY);
-			if(resultado == 1) {
-				limparTextoNovaReta();
-				controller->getView()->inserirTextoConsole("Nova reta adicionada.");
-			} else if(resultado == -1) {
-				controller->getView()->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
-			} else if(resultado == -2) {
-				controller->getView()->inserirTextoConsole("ERRO: coordenadas devem ser valores numéricos.");
-			} else if(resultado == -3) {
-				controller->getView()->inserirTextoConsole("ERRO: não é possível inserir reta sem dois pares de coordenadas.");
-			}
-			break;
-			}
-		case 2: {
-			// A page 2 corresponde à aba de Polígono
-			int resultado = displayFile->inserirNovoPoligono(nome, listaCoordsPoligono);
-			if(resultado == 1) {
-				limparTextoNovoPoligono();
-				listaCoordsPoligono = new ListaEnc<Coordenada*>();
-				controller->getView()->inserirTextoConsole("Novo poligono adicionado.");
-			} else if(resultado == -1) {
-				controller->getView()->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
-			} else if(resultado == -3) {
-				controller->getView()->inserirTextoConsole("ERRO: não é possível inserir polígono sem coordenadas.");
-			}
-			break;
-			}
-	}
-	update_Surface();
-	gtk_widget_set_sensitive (poligono_Btn_Del, FALSE); // botão de deletar coordenada é desativado
-	gtk_widget_grab_focus((GtkWidget*) textoNomeElemento); // foco vai para a caixa de texto de nome do novo elemento
-	*/
+	controller->addNovoElemento();
 }
 
 
@@ -300,23 +119,7 @@ extern "C" G_MODULE_EXPORT void NovoElmnt_Adicionar_clicked_cb() {
 	Adiciona uma nova coordenada à lista de coordenadas e à list box.
 */
 extern "C" G_MODULE_EXPORT void NovoElmnt_Pol_Add_clicked_cb() {
-	try {
-		controller->getView()->inserirCoordListaEnc();
-		controller->getView()->limparTextoCoordPoligono();
-		controller->getView()->focusCoordPoligono();
-	} catch (...){
-		return;
-	}
-	/*
-	string c = inserirCoordListaEnc();
-	if ( !(c.empty()) )  {
-		addToListBox(poligono_Listbox, c);
-		gtk_entry_set_text(textoPoligonoX, "");
-		gtk_entry_set_text(textoPoligonoY, "");
-		// Dá foco à caixa de texto X
-		gtk_widget_grab_focus((GtkWidget*) textoPoligonoX);
-	}
-	*/
+	controller->addNovaCoordenadaPolinomio();
 }
 
 //! Método que é chamado ao pressionar o botão de deletar coordenada na criação de um polígono.
@@ -324,7 +127,7 @@ extern "C" G_MODULE_EXPORT void NovoElmnt_Pol_Add_clicked_cb() {
 	Remove a coordenada selecionada na list box de coordenadas do novo poligono.
 */
 extern "C" G_MODULE_EXPORT void NovoElmnt_Pol_Del_clicked_cb(){
-	controller->getView()->deletarCoordPoligono();
+	controller->delCoordenadaPolinomio();
 }
 
 //! Método que é chamado ao selecionar uma coordenada da list box de criação de poligono.
@@ -332,7 +135,7 @@ extern "C" G_MODULE_EXPORT void NovoElmnt_Pol_Del_clicked_cb(){
 	Ativa o botão de deletar coordenada.
 */
 extern "C" G_MODULE_EXPORT void NovoElmnt_Listbox_Pol_row_selected_cb (GtkListBox *box, GtkListBoxRow *row, gpointer user_data) {
-	controller->getView()->setPoligono_Btn_DelSensitive(TRUE);	
+	controller->selecionaCoordenadaListBox();
 }
 
 
@@ -341,5 +144,5 @@ extern "C" G_MODULE_EXPORT void NovoElmnt_Listbox_Pol_row_selected_cb (GtkListBo
 	Restaura a janela para o seu estado inicial.
 */
 extern "C" G_MODULE_EXPORT void Window_NovoElmnt_hide_cb(){
-	controller->getView()->resetarJanelaNovoElemento();
+	controller->janelaNovoElementoHide();
 }
--- Trabalho1/Controller.hpp
+++ Trabalho1/Controller.hpp
@@ -84,6 +84,202 @@ public:
 		model->fazTranslacao(elem, coord);	
 	}
 	
+	// Métodos chamados pela interface de usuário
+	
+	void janelaPrincipalDestroy() {
+		view->fecharPrograma();
+	}
+	
+	void selecionaElementoListBox() {
+		view->setElmnt_Btn_DelSensitive(TRUE);
+	}
+	
+	void botaoNovoElemento() {
+		view->Elmnt_Btn_Novo_Clicado();
+	}
+	
+	void botaoDeletarElemento() {
+		try {
+			int index = view->deletarElemento();
+			model->deletarElemento(index);
+			atualizaDesenho();
+			view->inserirTextoConsole("Elemento excluído.");
+			view->setElmnt_Btn_DelSensitive(FALSE);	
+		} catch (int erro) {
+			if (erro == 1) {
+				view->inserirTextoConsole("É preciso selecionar um elemento para ser deletado.");
+			}
+		}
+	}
+	
+	void botaoMovimentoCima() {
+		try {
+			double fator = view->getFatorMovimento();
+			model->moverWindow(0,fator);
+			atualizaDesenho();
+			view->inserirTextoConsole("Movimentação para cima.");
+		} catch (...){
+			return;
+		}
+	}
+	
+	void botaoMovimentoEsquerda() {
+		try {
+			double fator = view->getFatorMovimento();
+			model->moverWindow(-fator,0);
+			atualizaDesenho();
+			view->inserirTextoConsole("Movimentação para a esquerda.");
+		} catch (...){
+			return;
+		}
+	}
+	
+	void botaoMovimentoDireita() {
+		try {
+			double fator = view->getFatorMovimento();
+			model->moverWindow(fator,0);
+			atualizaDesenho();
+			view->inserirTextoConsole("Movimentação para a direita.");
+		} catch (...){
+			return;
+		}
+	}
+	
+	void botaoMovimentoBaixo() {
+		try {
+			double fator = view->getFatorMovimento();
+			model->moverWindow(0,-fator);
+			atualizaDesenho();
+			view->inserirTextoConsole("Movimentação para baixo.");
+		} catch (...){
+			return;
+		}
+	}
+	
+	void botaoZoomMenos() {
+		try {
+			double fator = view->getFatorZoom();
+			model->zoom(-fator);
+			atualizaDesenho();
+			view->inserirTextoConsole("Zoom para fora.");
+		} catch (...){
+			return;
+		}
+	}
+	
+	void botaoZoomMais() {
+		try {
+			double fator = view->getFatorZoom();
+			model->zoom(fator);
+			atualizaDesenho();
+			view->inserirTextoConsole("Zoom para dentro.");
+		} catch (...){
+			return;
+		}
+	}
+	
+	void drawingAreaConfigure(GtkWidget *widget) {
+		view->nova_surface(widget);
+		atualizaDesenho();
+	}
+	
+	void drawingAreaDraw(cairo_t *cr) {
+		view->modifica_surface(cr);
+	}
+	
+	void addNovoElemento() {
+		string nome = view->getNomeElemento();
+		int tipo = view->getTipoNovoElemento();
+		switch (tipo) {
+			case 0: { // A page 0 corresponde à aba de Ponto
+				string coordX = view->getCoordXNovoPonto();
+				string coordY = view->getCoordYNovoPonto();
+				try {
+					model->inserirNovoPonto(nome, coordX, coordY);
+					view->limparTextoNovoPonto();
+					view->adicionaElementoListbox(nome);
+					view->inserirTextoConsole("Novo ponto adicionado.");
+				} catch (int erro) {
+					if (erro == -1) {
+						view->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
+					} else if (erro == -2) {
+						view->inserirTextoConsole("ERRO: coordenadas devem ser valores numéricos.");
+					} else if (erro == -3) {
+						view->inserirTextoConsole("ERRO: não é possível inserir um ponto sem coordenadas.");
+					}
+				}
+				
+				break;
+			} case 1: { // A page 1 corresponde à aba de Reta
+				string coordIniX = view->getCoordIniXNovaReta();
+				string coordIniY = view->getCoordIniYNovaReta();
+				string coordFinX = view->getCoordFinXNovaReta();
+				string coordFinY = view->getCoordFinYNovaReta();
+			
+				try {
+					model->inserirNovaReta(nome, coordIniX, coordIniY, coordFinX, coordFinY);
+					view->limparTextoNovaReta();
+					view->adicionaElementoListbox(nome);
+					view->inserirTextoConsole("Nova reta adicionada.");
+				} catch (int erro) {
+					if (erro == -1) {
+						view->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
+					} else if (erro == -2) {
+						view->inserirTextoConsole("ERRO: coordenadas devem ser valores numéricos.");
+					} else if (erro == -3) {
+						view->inserirTextoConsole("ERRO: não é possível inserir reta sem dois pares de coordenadas.");
+					}
+				}
+				
+				break;
+			} case 2: { // A page 2 corresponde à aba de Polígono
+				ListaEnc<Coordenada*>* lista = view->getListaCoordsPoligono();
+				
+				try {
+					model->inserirNovoPoligono(nome, lista);
+					view->resetarListaCoordenadasPoligono();
+					view->limparTextoNovoPoligono();
+					view->adicionaElementoListbox(nome);
+					view->inserirTextoConsole("Novo poligono adicionado.");
+				} catch (int erro) {
+					if (erro == -1) {
+						view->inserirTextoConsole("ERRO: não é possível inserir elemento sem nome.");
+					} else if (erro == -3) {
+						view->inserirTextoConsole("ERRO: não é possível inserir polígono sem coordenadas.");
+					}
+				}
+				
+				break;
+			}
+		}
+		
+		atualizaDesenho();
+		view->setPoligono_Btn_DelSensitive(FALSE);
+		view->focusNome();
+	}
+	
+	void addNovaCoordenadaPolinomio() {
+		try {
+			view->inserirCoordListaEnc();
+			view->limparTextoCoordPoligono();
+			view->focusCoordPoligono();
+		} catch (...){
+			return;
+		}
+	}
+	
+	void delCoordenadaPolinomio() {
+		view->deletarCoordPoligono();
+	}
+	
+	void selecionaCoordenadaListBox() {
+		view->setPoligono_Btn_DelSensitive(TRUE);	
+	}
+	
+	void janelaNovoElementoHide() {
+		view->resetarJanelaNovoElemento();
+	}
+	
 };
 
 #endif
